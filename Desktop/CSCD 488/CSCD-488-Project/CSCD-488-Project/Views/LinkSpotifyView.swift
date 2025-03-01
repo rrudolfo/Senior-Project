134a4374660907b2ad105081387026206b4a07f6
jacoblucas <jacoblucas1701@gmail.com>
2025-03-01 15:56:59 -0800

    Build methods to link user spotify account.

---
+++ Desktop/CSCD 488/CSCD-488-Project/CSCD-488-Project/Views/LinkSpotifyView.swift
@@ -0,0 +1,18 @@
+//
+//  LinkSpotifyView.swift
+//  CSCD-488-Project
+//
+//  Created by Jacob Lucas on 3/1/25.
+//
+
+import SwiftUI
+
+struct LinkSpotifyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LinkSpotifyView()
+}
---
+++ Desktop/CSCD 488/CSCD-488-Project/CSCD-488-Project/Views/LinkSpotifyViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LinkSpotifyViewModel.swift
+//  CSCD-488-Project
+//
+//  Created by Jacob Lucas on 3/1/25.
+//
+
+import Foundation
